85ada4357c9e89f31ad50b8941887ead92d41e66
Marina Dionicio <cmarcod@hotmail.com>
2024-11-17 15:55:19 -0500

    TenthCommit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log('1');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 console.log('2');
 app.use(cors());
 app.use(express.json());
