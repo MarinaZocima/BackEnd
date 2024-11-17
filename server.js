b736b921c034be2e0c7c772ea9d5eec389aa8b52
Marina Dionicio <cmarcod@hotmail.com>
2024-11-16 23:03:59 -0500

    SexthCommit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ console.log('1');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 console.log('2');
 app.use(cors());
 app.use(express.json());
@@ -20,5 +20,5 @@ app.use('/api/workers', workerRoutes);
 app.use('/api/professions', professionRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
